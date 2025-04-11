f1d5fa87a6984062ebbe82d75de3aab2d8efeba0
Michael Brooks <mbrooks@slack-corp.com>
2025-04-11 09:31:10 -0700

    test: remove duplicate test for collaborator printSuccess

--- cmd/collaborators/collaborators_test.go
+++ cmd/collaborators/collaborators_test.go
@@ -22,9 +22,7 @@ import (
 	"github.com/slackapi/slack-cli/internal/prompts"
 	"github.com/slackapi/slack-cli/internal/shared"
 	"github.com/slackapi/slack-cli/internal/shared/types"
-	"github.com/slackapi/slack-cli/internal/slackcontext"
 	"github.com/slackapi/slack-cli/internal/slacktrace"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
@@ -133,31 +131,4 @@ func TestCollaboratorsCommand(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestCollaboratorsCommand_PrintSuccess(t *testing.T) {
-	// Setup
-	ctx := slackcontext.MockContext(t.Context())
-	clientsMock := shared.NewClientsMock()
-	clients := shared.NewClientFactory(clientsMock.MockClientFactory())
-
-	// Execute tests
-	t.Run("Username will be used if present", func(t *testing.T) {
-		user := types.SlackUser{Email: "joe.smith@company.com", ID: "U1234", PermissionType: types.OWNER}
-		printSuccess(ctx, clients.IO, user, "added")
-		assert.Contains(t, clientsMock.GetStdoutOutput(), "joe.smith@company.com successfully added as an owner collaborator on this app")
-	})
-
-	t.Run("User has no email set; fall back on user ID", func(t *testing.T) {
-		user := types.SlackUser{ID: "U1234", PermissionType: types.OWNER}
-		printSuccess(ctx, clients.IO, user, "removed")
-		assert.Contains(t, clientsMock.GetStdoutOutput(), "\nU1234 successfully removed as an owner collaborator on this app\n\n")
-	})
-
-	t.Run("Reader-type collaborator", func(t *testing.T) {
-		user := types.SlackUser{Email: "joe.smith@company.com", ID: "U1234", PermissionType: types.READER}
-		printSuccess(ctx, clients.IO, user, "updated")
-		assert.Contains(t, clientsMock.GetStdoutOutput(), "\njoe.smith@company.com successfully updated as a reader collaborator on this app\n\n")
-	})
-
 }
