d84ae050ad063db6e5b6fcdb0841c7d60d3ccddf
@zimeg <zim@o526.net>
2025-05-20 00:14:38 -0700

    fix: continue to install the prior 64 bit installations prior to next version

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -119,17 +119,21 @@ install_slack_cli() {
         #
 
         if [ "$(uname)" == "Darwin" ]; then
-            case "$(uname -m)" in
-                x86_64)
-                    slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_amd64.tar.gz"
-                    ;;
-                arm64 | aarch64)
-                    slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_arm64.tar.gz"
-                    ;;
-                *)
-                    slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_64-bit.tar.gz"
-                    ;;
-            esac
+            if version_lt "$SLACK_CLI_DEV_VERSION" "3.3.0"; then
+                slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_64-bit.tar.gz"
+            else
+                case "$(uname -m)" in
+                    x86_64)
+                        slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_amd64.tar.gz"
+                        ;;
+                    arm64 | aarch64)
+                        slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_arm64.tar.gz"
+                        ;;
+                    *)
+                        slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_macOS_64-bit.tar.gz"
+                        ;;
+                esac
+            fi
         elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
                 slack_cli_url="https://downloads.slack-edge.com/slack-cli/slack_cli_${SLACK_CLI_DEV_VERSION}_linux_64-bit.tar.gz"
         else
