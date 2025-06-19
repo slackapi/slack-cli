77a3d7df8976b1aded6eac500893bf0a8ebabdd9
@zimeg <zim@o526.net>
2025-06-18 23:34:35 -0700

    test: make the expected error states and test names match setup

--- internal/pkg/platform/activity_test.go
+++ internal/pkg/platform/activity_test.go
@@ -110,10 +110,10 @@ func TestPlatformActivity_StreamingLogs(t *testing.T) {
 		},
 		"should return error if session validation fails": {
 			Setup: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) context.Context {
-				cm.API.On("ValidateSession", mock.Anything, mock.Anything).Return(api.AuthSession{}, slackerror.New("boomsies"))
+				cm.API.On("ValidateSession", mock.Anything, mock.Anything).Return(api.AuthSession{}, slackerror.New("mock_broken_validation"))
 				return ctx
 			},
-			ExpectedError: slackerror.New("boomsies"),
+			ExpectedError: slackerror.New("mock_broken_validation"),
 		},
 		"should return error from Activity API request if TailArg is not set": {
 			Args: types.ActivityArgs{
@@ -157,13 +157,15 @@ func TestPlatformActivity_StreamingLogs(t *testing.T) {
 				}()
 				return ctx
 			},
+			ExpectedError: nil,
 			ExpectedAsserts: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) {
+				// with the above tail argument not included we call the method once.
 				cm.API.AssertNumberOfCalls(t, "Activity", 1)
 				assert.Contains(t, cm.GetStdoutOutput(), "[warn] [a123] (Trace=tr123)")
 				assert.Contains(t, cm.GetStdoutOutput(), "[error] [a456] (Trace=tr456)")
 			},
 		},
-		"should return nil and invoke Activity API twice if TailArg is set but idle": {
+		"should return nil and invoke Activity API twice if TailArg is set while polling": {
 			Args: types.ActivityArgs{
 				TailArg:           true,
 				IdleTimeoutM:      0,
@@ -178,28 +180,10 @@ func TestPlatformActivity_StreamingLogs(t *testing.T) {
 				}()
 				return ctx
 			},
-			ExpectedAsserts: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) {
-				cm.API.AssertNumberOfCalls(t, "Activity", 2)
-			},
-		},
-		"should continue if TailArg is set and activity request fails": {
-			Args: types.ActivityArgs{
-				TailArg:           true,
-				IdleTimeoutM:      1,
-				PollingIntervalMS: 20, // poll activity every 20 ms
-			},
-			Setup: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) context.Context {
-				cm.API.On("Activity", mock.Anything, mock.Anything, mock.Anything).Return(api.ActivityResult{}, slackerror.New("mock_broken_logs"))
-				ctx, cancel := context.WithCancel(ctx)
-				go func() {
-					time.Sleep(time.Millisecond * 50) // cancel activity in 50 ms
-					cancel()
-				}()
-				return ctx
-			},
 			ExpectedError: nil,
 			ExpectedAsserts: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) {
-				cm.API.AssertNumberOfCalls(t, "Activity", 3)
+				// with the above polling/canceling setup, expectation is activity called two times.
+				cm.API.AssertNumberOfCalls(t, "Activity", 2)
 			},
 		},
 		"should return nil if TailArg is set and context is canceled": {
@@ -216,9 +200,31 @@ func TestPlatformActivity_StreamingLogs(t *testing.T) {
 				}()
 				return ctx
 			},
+			ExpectedError: nil,
 			ExpectedAsserts: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) {
 				// with the above polling/canceling setup, expectation is activity called only once.
 				cm.API.AssertNumberOfCalls(t, "Activity", 1)
+			},
+		},
+		"should return nil if TailArg is set and activity request fails while polling": {
+			Args: types.ActivityArgs{
+				TailArg:           true,
+				IdleTimeoutM:      1,
+				PollingIntervalMS: 20, // poll activity every 20 ms
+			},
+			Setup: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) context.Context {
+				cm.API.On("Activity", mock.Anything, mock.Anything, mock.Anything).Return(api.ActivityResult{}, slackerror.New("mock_broken_logs"))
+				ctx, cancel := context.WithCancel(ctx)
+				go func() {
+					time.Sleep(time.Millisecond * 50) // cancel activity in 50 ms
+					cancel()
+				}()
+				return ctx
+			},
+			ExpectedError: nil,
+			ExpectedAsserts: func(t *testing.T, ctx context.Context, cm *shared.ClientsMock) {
+				// with the above polling/canceling setup, expectation is activity called three times.
+				cm.API.AssertNumberOfCalls(t, "Activity", 3)
 			},
 		},
 	} {
