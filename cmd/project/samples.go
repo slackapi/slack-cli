1d23e940ff1df85eb8e207bc5177341ba49bd174
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-09-19 14:59:06 -0700

    feat: output a list of samples for a language with the samples command (#192)
    Co-authored-by: Michael Brooks <mbrooks@slack-corp.com>

--- cmd/project/samples.go
+++ cmd/project/samples.go
@@ -15,9 +15,13 @@
 package project
 
 import (
+	"context"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/slackapi/slack-cli/internal/api"
+	"github.com/slackapi/slack-cli/internal/pkg/create"
 	"github.com/slackapi/slack-cli/internal/shared"
 	"github.com/slackapi/slack-cli/internal/style"
 	"github.com/spf13/cobra"
@@ -26,6 +30,7 @@ import (
 // Flags
 var samplesTemplateURLFlag string
 var samplesGitBranchFlag string
+var samplesListFlag bool
 var samplesLanguageFlag string
 
 func NewSamplesCommand(clients *shared.ClientFactory) *cobra.Command {
@@ -35,6 +40,10 @@ func NewSamplesCommand(clients *shared.ClientFactory) *cobra.Command {
 		Short:   "List available sample apps",
 		Long:    "List and create an app from the available samples",
 		Example: style.ExampleCommandsf([]style.ExampleCommand{
+			{
+				Meaning: "List Bolt for JavaScript samples",
+				Command: "samples --list --language node",
+			},
 			{
 				Meaning: "Select a sample app to create",
 				Command: "samples my-project",
@@ -50,6 +59,7 @@ func NewSamplesCommand(clients *shared.ClientFactory) *cobra.Command {
 	cmd.Flags().StringVarP(&samplesTemplateURLFlag, "template", "t", "", "template URL for your app")
 	cmd.Flags().StringVarP(&samplesGitBranchFlag, "branch", "b", "", "name of git branch to checkout")
 	cmd.Flags().StringVar(&samplesLanguageFlag, "language", "", "runtime for the app framework\n  ex: \"deno\", \"node\", \"python\"")
+	cmd.Flags().BoolVar(&samplesListFlag, "list", false, "prints samples without interactivity")
 
 	return cmd
 }
@@ -60,7 +70,18 @@ func runSamplesCommand(clients *shared.ClientFactory, cmd *cobra.Command, args [
 	sampler := api.NewHTTPClient(api.HTTPClientOptions{
 		TotalTimeOut: 60 * time.Second,
 	})
-	selectedSample, err := PromptSampleSelection(ctx, clients, sampler)
+	samples, err := create.GetSampleRepos(sampler)
+	if err != nil {
+		return err
+	}
+	if samplesListFlag || !clients.IO.IsTTY() {
+		err := listSampleSelection(ctx, clients, samples)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	selectedSample, err := promptSampleSelection(ctx, clients, samples)
 	if err != nil {
 		return err
 	}
@@ -84,4 +105,56 @@ func runSamplesCommand(clients *shared.ClientFactory, cmd *cobra.Command, args [
 
 	// Execute the `create` command with the set flag
 	return createCmd.ExecuteContext(ctx)
+}
+
+// listSampleSelection outputs available samples matching a language flag filter
+func listSampleSelection(ctx context.Context, clients *shared.ClientFactory, sampleRepos []create.GithubRepo) error {
+	filteredRepos := filterRepos(sampleRepos, samplesLanguageFlag)
+	sortedRepos := sortRepos(filteredRepos)
+	templateRepos := []create.GithubRepo{}
+	exampleRepos := []create.GithubRepo{}
+	for _, repo := range sortedRepos {
+		if strings.Contains(repo.FullName, "template") {
+			templateRepos = append(templateRepos, repo)
+		} else {
+			exampleRepos = append(exampleRepos, repo)
+		}
+	}
+	message := ""
+	if samplesLanguageFlag != "" {
+		message = fmt.Sprintf(
+			"Listing %d \"%s\" templates and project samples",
+			len(filteredRepos),
+			samplesLanguageFlag,
+		)
+	} else {
+		message = fmt.Sprintf("Listing %d template and project samples", len(filteredRepos))
+	}
+	clients.IO.PrintInfo(ctx, false, "\n%s", style.Sectionf(style.TextSection{
+		Emoji: "toolbox",
+		Text:  "Samples",
+		Secondary: []string{
+			message,
+		},
+	}))
+	samples := append(
+		templateRepos,
+		exampleRepos...,
+	)
+	for _, sample := range samples {
+		clients.IO.PrintInfo(ctx, false, style.Sectionf(style.TextSection{
+			Emoji: "hammer_and_wrench",
+			Text: fmt.Sprintf(
+				" %s | %s | %d %s",
+				style.Bold(sample.Name),
+				sample.Description,
+				sample.StargazersCount,
+				style.Pluralize("star", "stars", sample.StargazersCount),
+			),
+			Secondary: []string{
+				fmt.Sprintf("https://github.com/%s", sample.FullName),
+			},
+		}))
+	}
+	return nil
 }
