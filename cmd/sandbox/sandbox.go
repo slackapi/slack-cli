a50fb40ad8db468e1a684f3dc963a8a6abdeadee
Elaine Vegeris <evegeris@slack-corp.com>
2026-03-18 17:23:03 -0400

    Update cmd/sandbox/sandbox.go
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>

--- cmd/sandbox/sandbox.go
+++ cmd/sandbox/sandbox.go
@@ -77,6 +77,7 @@ func getSandboxAuth(ctx context.Context, clients *shared.ClientFactory) (*types.
 	}
 
 	// Prompt the user to select a team to use for authentication
+	// TODO(experiment:charm): Change this to prompt "help" message once charm is stable
 	clients.IO.PrintInfo(ctx, false, "%s", style.Secondary("Choose a Slack team where your email address matches your Slack developer account"))
 	auth, err := prompts.PromptTeamSlackAuth(ctx, clients, "Select a team for authentication")
 	if err != nil {
