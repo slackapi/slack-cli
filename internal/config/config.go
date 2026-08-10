b021d22be253247aa90ecbe8a14d331cdbf01f06
Ale Mercado <maria.mercado@slack-corp.com>
2026-08-10 13:49:44 -0400

    feat: add --manifest-source flag to run and deploy commands

--- internal/config/config.go
+++ internal/config/config.go
@@ -54,6 +54,7 @@ type Config struct {
 	DisableTelemetryFlag    bool
 	ForceFlag               bool
 	ForceRemoteFlag         bool
+	ManifestSourceFlag      string
 	LogstashHostResolved    string
 	NoColor                 bool
 	RuntimeFlag             string
